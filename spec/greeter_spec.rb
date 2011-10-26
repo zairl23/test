3fb552a7e919833db293d1767496de099d4c9482
zairl23 <756127792@qq.com>
2011-10-26 14:47:11 +0800

    a commit about class RspesGreeter

--- spec/greeter_spec.rb
+++ spec/greeter_spec.rb
@@ -1,3 +1,10 @@
+#follow the book, just type 'class RspecGreeter ...'
+# but rasie errors below:
+#Failure/Error: greeter = RSpecGreeter.new
+#  uninitialized constant RSpec::Core::ExampleGroup::Nested_1::RSpecGreeter
+# ./greeter_spec.rb:3:in `block (2 levels) in <top (required)>'
+#so i add this below '< RSpec::Core::ExampleGroup', and it works.why?
+
 class RSpecGreeter < RSpec::Core::ExampleGroup
   def greet
     "Hello Rspec!"
