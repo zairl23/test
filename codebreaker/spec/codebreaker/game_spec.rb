f89447722710c2f9f58d60dbbfb0d0cb68745a29
zairl23 <756127792@qq.com>
2011-10-28 16:17:44 +0800

    fix no method let

--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -3,10 +3,9 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
-      before(:each) do
-        let (:output) { double('output' ).as_null_object }
-        let (:game) { Game.new(output) }
-      end
+      let(:output) { double('output' ).as_null_object }
+      let(:game) { Game.new(output) }
+     
       
         it "sends a welcome message" do
           output.should_receive(:puts).with('Welcome to Codebreaker!' )
