c5fe9a5765a627bc09ecd7cd4b2502e7a894efe1
zairl23 <756127792@qq.com>
2011-11-08 16:44:07 +0800

    passed variable title

--- sample_app/spec/controllers/pages_controller_spec.rb
+++ sample_app/spec/controllers/pages_controller_spec.rb
@@ -5,23 +5,29 @@ describe PagesController do
 
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "have the right title" do
       get 'home'
-      response.should be_success
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | Home")
+
     end
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "have the right title" do
       get 'contact'
-      response.should be_success
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App  Contact")
+
     end
   end
   
   describe "GET 'about'" do
-    it "should be successful" do
+    it "have the right title" do
       get 'about'
-      response.should be_success
+      response.should have_selector("title",
+        :content => "Ruby on Rails Tutorial Sample App | About")
+
    end
   end
 
