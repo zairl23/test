601147ca21c0962222043779cdaa784ff644d47b
zairl23 <756127792@qq.com>
2011-11-12 17:45:52 +0800

    finish modeling User one

---
+++ sample_app/trick.rb
@@ -0,0 +1,19 @@
+#attr_accessbile :name, :email
+
+#rails c --sanbox
+
+#tail -f log/development.log
+
+#add_index :users, :email, :unique => true
+#remove_index :users ,:email
+
+#rake db:test:prepare
+
+#validates :name, :presence => true
+#                 :length => { :maximum => 50 }
+#                 :format => { :with => regex }
+
+#rspec
+#  before(:each) do
+  
+#  end 
