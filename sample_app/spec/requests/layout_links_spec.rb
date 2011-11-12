c22acd35eeb0bd555639aed092aae57b84d837f1
zairl23 <756127792@qq.com>
2011-11-12 15:39:16 +0800

    finish layout_link_spec

--- sample_app/spec/requests/layout_links_spec.rb
+++ sample_app/spec/requests/layout_links_spec.rb
@@ -20,7 +20,12 @@ require 'spec_helper'
       get '/help'
       response.should have_selector('title', :content => "Help")
     end
- 
+   
+    it "should have a signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector('title', :content => "Sign up")
+    end
+
 
 end
 
