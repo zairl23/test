0b61c911e842809848610877941ff27708ad7c00
zairl23 <756127792@qq.com>
2011-11-14 01:40:28 +0800

    User model with passwords, and user show page

--- sample_app/spec/controllers/users_controller_spec.rb
+++ sample_app/spec/controllers/users_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe UsersController do
+  render_views
 
   describe "GET 'new'" do
     it "returns http success" do
@@ -8,5 +9,21 @@ describe UsersController do
       response.should be_success
     end
   end
+  
+  describe "GET 'show'" do
+    before(:each) do
+      @user = Factory(:user)
+    end
+    
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
 
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+
+  end
 end
--- sample_app/spec/models/user_spec.rb
+++ sample_app/spec/models/user_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
   before(:each) do
-    @attr = { :name => "neychang", :email => "zairl23@126.com" } 
+    @attr = { :name                  => "neychang", 
+              :email                 => "zairl23@126.com", 
+              :password              => "neychang", 
+              :password_confirmation => "neychang"
+             } 
   end
+ 
+
   
   it "should require an email address" do
     no_email_user = User.new(@attr.merge(:email => ''))
@@ -39,4 +45,68 @@ describe User do
     user_with_duplicate_email.should_not be_valid
   end
 
+  it "should have virlua attribute password" do
+    user = User.new(@attr)
+    user.should respond_to(:password)
+  end
+  
+  it "should require a password" do
+    no_password_user = User.new(@attr.merge(:password => '', 
+                              :password_confirmation => ''
+                              ))
+    no_password_user.should_not be_valid
+  end
+  
+  it "should reject user about password's length" do
+    invalid_password = 'a' * 51
+    invalid_password_confirmation = 'a' * 51
+    user = User.new(@attr.merge(:password => invalid_password,
+                                :password_confirmation => invalid_password_confirmation
+                               ))
+    user.should_not be_valid
+  end
+  
+  it "should have an encrypted password attribute" do
+    user = User.new(@attr)
+    user.should respond_to(:encrypted_password)
+  end
+
+  describe "encrypted password" do
+    before(:each) do 
+      @user = User.create!(@attr)
+    end
+  
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    describe "has_password? method" do
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+    
+    describe "authenticate method" do
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+    
+      it "should return nil for an email address with no user" do
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistent_user.should be_nil
+      end
+    
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
+
+
+  end
 end
