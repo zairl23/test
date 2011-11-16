9b1b64319a2e3d0941c35709e0c3be49a82556fc
zairl23 <756127792@qq.com>
2011-11-16 14:19:01 +0800

    finish signing up

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
  
   def show
     @user = User.find(params[:id])
     @title = 'Sign up'
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome"
+
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
+  
   
 end
