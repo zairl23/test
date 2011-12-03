37eaba14dc62faa219c1ea6c942c3cc4dd8ceff4
zairl23 <756127792@qq.com>
2011-12-03 23:45:38 +0800

    som

---
+++ ruby/spec/ruby_numberic_spec.rb
@@ -0,0 +1,33 @@
+describe "numberic method % " do
+  it "should eql?" do
+    ((-1 % 3) == 1 % (-3)).should == true
+  end
+end
+
+describe "Select methods" do
+
+end
+
+describe "Add methods" do
+
+end
+
+describe "Deltete methods" do
+
+end
+
+describe "Convert methods" do
+
+end
+
+describe "Destroy methods" do
+
+end
+
+describe "Iterator methods" do
+
+end
+
+describe "Question methods" do
+
+end
